c8835447740f58b6da63e0bb9865fc6ecc1b15c5
Shri <me@drone-ah.com>
2025-06-29 16:44:18 +0100

    feat: ball now collides with top & bottom

--- games/pong/src/ball.zig
+++ games/pong/src/ball.zig
@@ -7,7 +7,7 @@ const Ball = @This();
 
 pos: rl.Vector2,
 r: f32 = 16,
-vel: rl.Vector2 = .{ .x = 250, .y = 0 },
+vel: rl.Vector2 = .{ .x = 250, .y = 300 },
 
 pub fn render(self: Ball) void {
     rl.drawCircleV(self.pos, self.r, .white);
@@ -21,5 +21,11 @@ pub fn update(self: *Ball, dt: f32) void {
 pub fn checkPaddleCollision(self: *Ball, paddle: *Paddle) void {
     if (paddle.isColliding(self)) {
         self.vel = rl.math.vector2Scale(self.vel, -1);
+    }
+}
+
+pub fn checkEdgeCollisions(self: *Ball, screen_height: f32) void {
+    if (self.pos.y < self.r or self.pos.y > screen_height - self.r) {
+        self.vel.y *= -1;
     }
 }
